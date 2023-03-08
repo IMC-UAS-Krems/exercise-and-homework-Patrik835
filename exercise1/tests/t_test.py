e08270ae9cf9a733d3c0a3b6fdb3f95445b26853
Patrik835 <114472655+Patrik835@users.noreply.github.com>
2023-03-08 19:27:31 +0100

    Exercise 1, 2.

--- exercise1/tests/t_test.py
+++ exercise1/tests/t_test.py
@@ -1,8 +1,6 @@
 import sqlite3
 import os
 
-
-
 def square(f):
     return f*f
 def test_square():
@@ -12,21 +10,17 @@ def pass_():
     return True
 def test_pass():
     assert pass_()
-def check_e():
-    try:
-        with open('exercise1/exercise1.db'):
-            pass
-    except FileNotFoundError:
-        "No file"
-    return "File exists"
-def test_check():
-    if "No file" in check_e():
-        assert False
-    else:
-        assert True
 
-# def test_database_file_exists():
-#     assert os.path.exists('exercise1/exercise1.db')
+def test_file_exist_no_lib():
+    try:
+        with open("../exercise1.db") as f:
+            assert True
+    except FileNotFoundError:
+        assert False
+
+# def test_file_exists():                            #alternative using os lib
+#     path = "../exercise1.db"
+#     assert os.path.exists(path)
 
 # def test_list_tables():
 #     conn = sqlite3.connect('exercise1.db')
