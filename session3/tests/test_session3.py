fe607bf545a22209aa7a0ef7eb6986bb95b6e670
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-03-29 13:03:43 +0200

    session3

--- session3/tests/test_session3.py
+++ session3/tests/test_session3.py
@@ -3,9 +3,13 @@ from session3 import create_db
 import pytest
 
 @pytest.fixture
-def test_database():
+def f_database():
     create_db("session3.db")
     conn = sqlite3.connect("session3.db")
     c= conn.cursor()
     c.execute("INSERT INTO Professor VALUES(123, 'name', 'g101','ss')")
-    
+def test_1(f_database):
+    conn = sqlite3.connect("session3.db")
+    c= conn.cursor()
+    c.execute("SELECT * FROM Professor")
+    assert c.fetchone() == (123, 'name', 'g101','ss')
